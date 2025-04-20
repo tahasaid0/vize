ad02ce84150ec0e1257f4325f1ba59de96bf38ba
taha sait barışkan <144110433+tahasaid0@users.noreply.github.com>
2025-04-20 21:01:36 +0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Veri Uygulaması',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: HomePage(),
+    );
+  }
+}
